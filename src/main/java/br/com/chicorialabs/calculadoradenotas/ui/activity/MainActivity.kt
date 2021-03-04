7f9bcfc88bebf7315592f4e4a497ba0a834cb655
chicorasia <chicorasia@gmail.com>
2021-03-04 18:25:05 -0300

    Refatorado MainActivity.xml

--- src/main/java/br/com/chicorialabs/calculadoradenotas/ui/activity/MainActivity.kt
+++ src/main/java/br/com/chicorialabs/calculadoradenotas/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
-package br.com.chicorialabs.calculadoradenotas
+package br.com.chicorialabs.calculadoradenotas.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.chicorialabs.calculadoradenotas.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
